cd8b9a186fd324f79a3b85504800123f93e72a86
sergeimitrochenko <aron.armenyan@yandex.ru>
2024-12-22 19:20:54 +0300

    "Задача № 1 и №2"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
+        String[] names = {"0", "1", "2"};
+        System.out.println(names[0]);
         System.out.println("Conichiva SEMPAIIIIII !");
+
+        System.out.println("Задача № 1");
+        int[] salary = {200_000, 15_000, 35_000, 145_000, 85_000};
+        int sum = 0;
+        for (int j : salary) {
+            sum += j;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+        System.out.println();
+
+        System.out.println("Задача № 2");
+        int Min = salary[0];
+        int Max = salary[1];
+        for (int j : salary) {
+            if (j > Max) {
+                Max = j;
+            }
+            if (j < Min) {
+                Min = j;
+            }
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " + Min + " рублей. Максимальная сумма трат за неделю составила " + Max + " рублей");
+        System.out.println();
     }
 }
+
